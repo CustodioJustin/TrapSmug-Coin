4e7ee624caa8b71d957ef1580c046532ee4fe3f0
CustodioJustin <justin.chyle.custodio@adamson.edu.ph>
2024-03-01 05:52:19 +0800

    initial implementation and handler revision

--- dapp/app/handlers/Wallet.ts
+++ dapp/app/handlers/Wallet.ts
@@ -2,12 +2,8 @@ import { useState } from "react";
 
 export async function ConnectWallet() {
     const { ethereum } = window as any;
-    try {
-        const accounts = await ethereum.request({
-            method: "eth_requestAccounts",
-        });
-        return { account: accounts[0], isConnected: true };
-    } catch (error) {
-        return { account: null, isConnected: false };
-    }
+    const accounts = await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    return accounts[0];
 }
--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -1,5 +1,27 @@
+"use client";
 import Image from "next/image";
+import { ConnectWallet } from "./handlers/Wallet";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
-  
+
+  const [walletKey, setwalletKey] = useState("");
+
+  const Connect = async () => {
+    const setwalletKey = await ConnectWallet();
+  }
+
+  return (
+    <main className="">
+      <button
+        onClick={() => {
+          Connect();
+        }}
+        className="p-3 bg-slate-800 text-white rounded"
+      >
+        {walletKey != "" ? walletKey : " Connect wallet"}
+      </button>
+    </main>
+  );
 }
